6658dda5070a22ffbe68d9c6ec9301a5ff4bc755
cam <123508794+fullstacknyc@users.noreply.github.com>
2024-10-15 15:57:11 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,25 +1,23 @@
 // src/firebaseConfig.js
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore} from "firebase/firestore";
+// Import the necessary Firebase modules
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration (replace these with your own from Firebase console)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjVO5ExFc50dj_CE8aH3_cfZocumKi28U",
-  authDomain: "household-items-app.firebaseapp.com",
-  projectId: "household-items-app",
-  storageBucket: "household-items-app.appspot.com",
-  messagingSenderId: "171173367576",
-  appId: "1:171173367576:web:375ad563903a8e0b02586d",
-  measurementId: "G-BQFWL73PM7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // Ensure you use environment variables for security
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
+// Initialize Firestore
 const db = getFirestore(app);
 
-export default app;
 export { db };

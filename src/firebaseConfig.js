fdbf022761dde7553b82796d4ef1c4c55eab168e
cam <123508794+fullstacknyc@users.noreply.github.com>
2024-10-15 16:26:21 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // Ensure you use environment variables for security
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBjV05ExFc50dj_CE8H3_cfZocumKi28U" , // Ensure you use environment variables for security
+  authDomain: "household-items-app.firebaseapp.com" ,
+  projectId: "household-items-app" ,
+  storageBucket: "household-items-app.appspot.com" ,
+  messagingSenderId: "1:171173367576:web:375ad563903a8e0b02586d" ,
+  appId: "G-BQFWL73PM7" ,
 };
 
 // Initialize Firebase
